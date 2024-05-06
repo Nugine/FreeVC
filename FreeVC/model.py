0903f28b0fd42d9996826b573539beef2d95b272
Nugine <nugine@foxmail.com>
2024-05-06 21:50:20 +0800

    u

--- FreeVC/model.py
+++ FreeVC/model.py
@@ -123,6 +123,8 @@ class MyLightningCLI(LightningCLI):
         for field in dataclasses.fields(DataConfig):
             key = field.name
             parser.link_arguments(f"data.config.{key}", f"model.config.data.{key}")
+        parser.set_defaults({"trainer.enable_checkpointing": True})
+        parser.set_defaults({"trainer.max_epochs": 10})
 
 
 if __name__ == "__main__":
